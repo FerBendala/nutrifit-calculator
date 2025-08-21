c177e0f491d97258fab8510bb5cc38639b65c0e3
Fernando Bendala <fernandobb@ext.inditex.com>
2025-08-21 14:33:17 +0200

    feat: añadir Google Tag Manager completo en layout
    - Añadir script GTM en head para tracking inmediato
    - Incluir noscript iframe para usuarios sin JavaScript
    - Configuración condicional basada en NEXT_PUBLIC_GTM_ID
    - Implementación estándar de GTM recomendada por Google
    - Preparar analytics y remarketing para AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,12 @@ export default function RootLayout({
           />
         )}
         {/* GTM will be loaded by consent banner if accepted */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <script>
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            }`}
+          </script>
+        )}
         {/* AdSense will be loaded by consent banner if accepted */}
       </head>
       <body className={inter.className}>
@@ -86,6 +92,9 @@ export default function RootLayout({
         </div>
         <ConsentBanner />
         <Toaster />
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`} height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        )}
       </body>
     </html>
   );
